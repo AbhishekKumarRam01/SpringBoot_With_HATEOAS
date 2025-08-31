3524ce2e9b7ddb586f29c33e1e43d15d84219bb2
Abhishek <ak2162586@gmail.com>
2025-08-31 14:54:09 +0530

    LOMBOK add 3rd time update

--- src/main/java/com/abhi/it/entity/User.java
+++ src/main/java/com/abhi/it/entity/User.java
@@ -2,43 +2,15 @@ package com.abhi.it.entity;
 
 import org.springframework.hateoas.RepresentationModel;
 
+import lombok.Data;
 
+@Data
 public class User extends RepresentationModel<User>{
 	private  Integer Id;
 	private String name;
 	private String email;
 	
-	public User() {
-		super();
-	}
-	public User(Integer id, String name, String email) {
-		super();
-		Id = id;
-		this.name = name;
-		this.email = email;
-	}
-	public Integer getId() {
-		return Id;
-	}
-	public void setId(Integer id) {
-		Id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	@Override
-	public String toString() {
-		return "User [Id=" + Id + ", name=" + name + ", email=" + email + "]";
-	}
+
 	
 	
  
